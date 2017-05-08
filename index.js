aafc55073b04218f3d861fb2af2dfad89a53c0a3
jamescross91 <james_cross91@hotmail.com>
2017-05-08 14:16:38 +0100

    Use env vars for PG user and PG pass, tidy up

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var AWS = require('aws-sdk');
-var redshift = require("./redshift-archiver")
-var dateFormat = require('dateformat');
+const AWS = require('aws-sdk');
+const redshift = require("./redshift-archiver");
+const dateFormat = require('dateformat');
 
 AWS.config.loadFromPath('./config.json');
-var docClient = new AWS.DynamoDB.DocumentClient();
-var table = "cube-archive-configuration";
-var params = {
+const docClient = new AWS.DynamoDB.DocumentClient();
+const table = "cube-archive-configuration";
+const params = {
     TableName: table,
     ProjectionExpression: "table_name, timestamp_col"
 };
 
-console.log("Cuttoff date is " + get_cuttoff_date())
+console.log("Cuttoff date is " + get_cuttoff_date());
 console.log("Scanning archive configuration table");
 
 docClient.scan(params, onScan);
@@ -30,10 +30,10 @@ function onScan(err, data) {
 }
 
 function get_cuttoff_date() {
-    var curr_date = new Date();
-    var new_date = new Date(curr_date)
+    let curr_date = new Date();
+    let new_date = new Date(curr_date);
 
-    new_date.setFullYear(curr_date.getFullYear() -2)
+    new_date.setFullYear(curr_date.getFullYear() -2);
 
     return dateFormat(new_date, "yyyy-mm-dd");
 }
--- redshift-archiver.js
+++ redshift-archiver.js
@@ -1,23 +1,23 @@
-var pg = require('pg');
+const pg = require('pg');
 
-var config = {
-    user: 'croud_kevin',
-    database: '', //env var: PGDATABASE
-    password: '', //env var: PGPASSWORD
-    host: '', // Server hosting the postgres database
+const config = {
+    user: process.env.PGUSER,
+    database: 'cubeprojectdb', //env var: PGDATABASE
+    password: process.env.PGPASSWORD,
+    host: 'regus-cube.cwcclxyklvva.eu-west-1.redshift.amazonaws.com', // Server hosting the postgres database
     port: 5439, //env var: PGPORT
     max: 1, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
 exports.archive = function(table_name, tstamp_col, date_threshold) {
-    var client = new pg.Client(config);
+    let client = new pg.Client(config);
     client.connect(function (err) {
         if (err) {
             return console.error('could not connect to postgres', err);
         }
 
-        var sql = `unload \
+        let sql = `unload \
         ('select * from ${table_name} where date_cmp_timestamp(''${date_threshold}'', ${tstamp_col}) = 1') \
         to 's3://regus-cube-archive/${table_name}/${date_threshold}' \
         iam_role 'arn:aws:iam::114196566689:role/redshift-s3-full'`;
@@ -29,20 +29,20 @@ exports.archive = function(table_name, tstamp_col, date_threshold) {
                 console.error('error running query', err);
             }
             console.info("redshift load: no errors, seem to be successful, will try to dump!");
-            delete_from_redshift(table_name, tstamp_col, date_threshold)
+            delete_from_redshift(table_name, tstamp_col, date_threshold);
             client.end();
         });
-    })
+    });
 }
 
 delete_from_redshift = function(table_name, tstamp_col, date_threshold) {
-    var client = new pg.Client(config);
+    let client = new pg.Client(config);
     client.connect(function (err) {
         if (err) {
             return console.error('could not connect to postgres', err);
         }
 
-        var sql = `delete from ${table_name} where date_cmp_timestamp('${date_threshold}', ${tstamp_col}) = 1;`;
+        let sql = `delete from ${table_name} where date_cmp_timestamp('${date_threshold}', ${tstamp_col}) = 1;`;
 
         console.log("Running " + sql);
 
@@ -53,5 +53,5 @@ delete_from_redshift = function(table_name, tstamp_col, date_threshold) {
             console.info("redshift delete: no errors, seem to be successful!");
             client.end();
         });
-    })
+    });
 }
